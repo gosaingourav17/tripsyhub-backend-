289332336ba4ec4427224fdd9d284ad0f9c9669d
Gourav gosain <40332708+gosaingourav17@users.noreply.github.com>
2018-09-15 18:20:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const http = require('http');
 const app = require('./app');
 const port = 5000;
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, function() {
+  console.log("Node app is running at localhost:")
+}));
